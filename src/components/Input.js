d86f7f2e30af052149d466ddb8be170c23e28aa9
Lootfi <lotfiholmes@hotmail.com>
2020-02-25 15:01:24 +0100

    storing event info in state works now

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,18 +1,19 @@
-import React, {useState} from 'react'
-import TextField from '@material-ui/core/TextField';
-import { ThemeProvider } from '@material-ui/styles';
-import theme from '../helpers/theme'
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "../helpers/theme";
 
-
-const Input = ({handleInputChange}) => {
-
-
-return(
-		<ThemeProvider theme={theme}>
-			<TextField onChange={e => handleInputChange(e)} id="basicInput" label="Input" variant="outlined" color="primary" />
-		</ThemeProvider>
-
-	)
-
-}
-export default Input
+const Input = ({ handleInputChange }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <TextField
+        onChange={e => handleInputChange(e)}
+        id="basicInput"
+        label="Input"
+        variant="outlined"
+        color="primary"
+      />
+    </ThemeProvider>
+  );
+};
+export default Input;
