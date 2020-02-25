f1f14d76881da30a7e3c7fabbaea19968b009afc
Lootfi <lotfiholmes@hotmail.com>
2020-02-25 17:02:52 +0100

    centered card child elems

--- src/components/ClientCard.js
+++ src/components/ClientCard.js
@@ -1,26 +1,31 @@
-import React from 'react'
-import Clients from './Clients'
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Avatar from '@material-ui/core/Avatar';
+import React from "react";
+import Clients from "./Clients";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Avatar from "@material-ui/core/Avatar";
+import { makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles(theme => ({
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  }
+}));
 
-const ClientCard = () => (
+export default function ClientCard() {
+  const classes = useStyles();
 
+  return (
+    <Card style={{ width: 400 }}>
+      <CardActions className={classes.card}>
+        <Avatar src="images/Logo.png" />
 
-    <Card style={{width: 400}} >
-    <CardActionArea>
-      <Avatar src="images/Logo.png" />
-      
-      <CardContent>
-        Lizard
-      </CardContent>
-    </CardActionArea>
+        <CardContent>Lizard</CardContent>
+      </CardActions>
     </Card>
-
-)
-
-export default ClientCard;
+  );
+}
